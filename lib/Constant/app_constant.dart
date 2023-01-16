05c928ca3d788998e9e41d2d14ccd80d1986b263
Bhavin8289 <bupadhyay277@gmail.com>
2023-01-16 14:27:24 +0530

    App Color

---
+++ lib/Constant/app_constant.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+import 'package:hexcolor/hexcolor.dart';
+
+final Color mainHexColor = HexColor("#999999");
+final Color accentHexColor = HexColor("#FC1C1C");
